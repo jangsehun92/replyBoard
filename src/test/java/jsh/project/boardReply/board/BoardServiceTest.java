9f9ce766f267d145fc401f4ca2df6c9e01c73ea4
jangsehun92 <jangsehun92@gmail.com>
2020-08-14 21:40:36 +0900

    boardService testcode 작성

--- src/test/java/jsh/project/boardReply/board/BoardServiceTest.java
+++ src/test/java/jsh/project/boardReply/board/BoardServiceTest.java
@@ -23,6 +23,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jsh.project.boardReply.board.dao.BoardDao;
+import jsh.project.boardReply.board.model.dto.request.RequestCreateArticleDto;
+import jsh.project.boardReply.board.model.dto.request.RequestEditArticleDto;
+import jsh.project.boardReply.board.model.dto.request.RequestRemoveArticleDto;
 import jsh.project.boardReply.board.model.dto.response.ResponseArticleDto;
 import jsh.project.boardReply.board.model.dto.response.ResponseBoardDto;
 import jsh.project.boardReply.board.service.BoardService;
@@ -44,16 +47,16 @@ public class BoardServiceTest {
 	public void 단일_게시글_가져오기() {
 		// given
 		int articleId = 12;
-		
+
 		ResponseArticleDto responseArticleDto = new ResponseArticleDto();
 		responseArticleDto.setId(12);
 		responseArticleDto.setTitle("test_title");
 		responseArticleDto.setContent("test_content");
 		responseArticleDto.setReplyCount(0);
 		responseArticleDto.setRegdate(new Date());
-		
+
 		given(boardDao.selectArticle(articleId)).willReturn(responseArticleDto);
-		
+
 		// when
 		ResponseArticleDto dto = boardService.getArticle(articleId);
 
@@ -67,10 +70,10 @@ public class BoardServiceTest {
 		// given
 		int page = 1;
 		given(boardDao.selectArticleTotalCount()).willReturn(11);
-		
+
 		// when
 		ResponseBoardDto dto = boardService.getArticles(page);
-		
+
 		// then
 		verify(boardDao, times(1)).selectArticles(any());
 		assertThat(dto.getPagination().getTotalPage(), is(2));
@@ -79,31 +82,43 @@ public class BoardServiceTest {
 	@Test
 	public void 게시글_입력() {
 		// given
+		RequestCreateArticleDto dto = new RequestCreateArticleDto();
+		dto.setTitle("insertTest");
+		dto.setContent("insertTest");
 
 		// when
+		boardService.createArticle(dto);
 
 		// then
-
+		verify(boardDao, times(1)).insertArticle(any());
 	}
 
 	@Test
 	public void 게시글_수정() {
 		// given
+		RequestEditArticleDto dto = new RequestEditArticleDto();
+		dto.setId(12);
+		dto.setTitle("updateTest");
+		dto.setContent("updateTest");
 
 		// when
+		boardService.editArticle(dto);
 
 		// then
-
+		verify(boardDao, times(1)).updateArticle(any());
 	}
 
 	@Test
 	public void 게시글_삭제() {
 		// given
-
+		RequestRemoveArticleDto dto = new RequestRemoveArticleDto();
+		dto.setId(12);
+		
 		// when
+		boardService.removeArticle(dto);
 
 		// then
-
+		verify(boardDao, times(1)).deleteArticle(any());
 	}
 
 }
